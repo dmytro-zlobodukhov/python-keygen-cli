e5663de6ca2867ddc8a9a85ee31c6360d70f2148
Dmytro Zlobodukhov <96909881+dmytro-zlobodukhov@users.noreply.github.com>
2024-08-28 18:43:14 +0200

    fixed 'requests.exceptions.InvalidHeader: Invalid leading whitespace, reserved character(s), or return character(s) in header value'

--- src/keygen_cli/config.py
+++ src/keygen_cli/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-API_BASE_URL = os.environ.get("KEYGEN_API_BASE_URL", "https://api.keygen.sh/v1")
-KEYGEN_PRODUCT_TOKEN = os.environ.get("KEYGEN_PRODUCT_TOKEN", "")
-ACCOUNT_ID = os.environ.get("KEYGEN_ACCOUNT_ID", "")
+API_BASE_URL = os.environ.get("KEYGEN_API_BASE_URL", "https://api.keygen.sh/v1").strip()
+KEYGEN_PRODUCT_TOKEN = os.environ.get("KEYGEN_PRODUCT_TOKEN", "").strip()
+ACCOUNT_ID = os.environ.get("KEYGEN_ACCOUNT_ID", "").strip()
 
 HEADERS = {
     "Authorization": f"Bearer {KEYGEN_PRODUCT_TOKEN}",
